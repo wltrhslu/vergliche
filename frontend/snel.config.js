2f1aa59c0762fc79b4971786d3770a37b9348954
Mirco <mirco.walter@stud.hslu.ch>
2022-02-17 23:06:10 +0100

    switched to react

--- frontend/snel.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  port: 6969,
-  mode: "dom",
-  plugins: [],
-  extendsImportMap: [],
-};
---
+++ frontend/src/App.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends React.Component {
+	async componentDidMount() {
+		const configs = await (await fetch("http://localhost:8080/config")).json();
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.tsx</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</header>
+			</div>
+		);
+	}
+}
+
+export default App;
