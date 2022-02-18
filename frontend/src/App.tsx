b69517a77be6df6e4ff1ab4bb2abdcd5a6f1d460
Mirco <mirco.walter@stud.hslu.ch>
2022-02-18 19:39:45 +0100

    switch to functional components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,49 @@
-import React from "react";
+import { FC, useEffect, useState } from "react";
 import logo from "./logo.svg";
+import ConfigComponent from "./components/Config";
 import "./App.css";
+import { IVendor, IVendorList } from "./interfaces/config";
 
-class App extends React.Component {
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.tsx</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</header>
-			</div>
-		);
-	}
+const App: FC = () => {
+	const [vendors, setVendors] = useState({
+		length: 0,
+		data: new Array<IVendor>(),
+	});
 
-	async componentDidMount() {
-		const configs = await (await fetch("http://localhost:8080/config")).json();
-	}
-}
+	useEffect(() => {
+		const getData = async () => {
+			const data = (await (
+				await fetch("http://localhost:8080/vendor")
+			).json()) as IVendorList;
+
+			setVendors({
+				length: data.length,
+				data: data.data,
+			});
+		};
+
+		getData();
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+			<ConfigComponent data={vendors.data} length={vendors.length} />
+		</div>
+	);
+};
 
 export default App;
