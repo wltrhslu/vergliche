fdfa8a5b990e91e17640f38c81cfff3cf85ef5d5
Mirco <mirco.walter@stud.hslu.ch>
2022-03-01 12:59:54 +0100

    include cheapest product router

--- backend/main.ts
+++ backend/main.ts
@@ -11,6 +11,7 @@ import ConfigRouter from "./routes/config.ts";
 import vendorRouter from "./routes/vendor.ts";
 import vendorCategoryRouter from "./routes/vendor-category.ts";
 import ServerSentEventRouter from "./routes/sse.ts";
+import CheapestProducts from "./routes/cheapestProducts.ts";
 import SearchService from "./services/search.ts";
 
 const port = 8080;
@@ -41,6 +42,8 @@ app.use(vendorRouter.allowedMethods());
 app.use(vendorRouter.routes());
 app.use(vendorCategoryRouter.allowedMethods());
 app.use(vendorCategoryRouter.routes());
+app.use(CheapestProducts.allowedMethods());
+app.use(CheapestProducts.routes());
 
 app.addEventListener("listen", () => {
 	console.log(`Listening on localhost:${port}`);
