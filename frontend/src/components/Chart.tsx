d7ad84ece92eca43f4d8ca1b0a087efc18c3f2fc
Mirco <mirco.walter@gmail.com>
2022-03-02 01:17:43 +0100

    added sse, start tooltip

--- frontend/src/components/Chart.tsx
+++ frontend/src/components/Chart.tsx
@@ -1,121 +1,79 @@
-import React, { useContext, useEffect, useState } from "react";
-import { FC } from "react";
+import { useEffect, useState, FC } from "react";
 import { IConfig, IProduct } from "../interfaces/config";
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const serverUrl = "http://localhost:8080";
 
 const Chart: FC<{ configId: number }> = (props) => {
 	const [config, setConfig] = useState({} as IConfig);
-	const [cheapestProducts, setCheapestProducts] = useState([{}] as IProduct[]);
+	const [chartData, setchartData] = useState(
+		{} as {
+			datasets: [{ data: IProduct[] }];
+		}
+	);
 
-	// const vendors = useContext(VendorContext);
-
-	// const [categories, setCategories] = useState(new Array<ICategory>());
+	const options: any = {
+		plugins: {
+			legend: {
+				display: false,
+			},
+			tooltip: {
+				callbacks: {
+					title: (context: any) => context[0].dataset.data[context[0].dataIndex].productName,
+					afterTitle: (context: any) => context[0].dataset.data[context[0].dataIndex].brandName,
+					label: (context: any) => context[0].dataset.data[context[0].dataIndex].brandName,
+				},
+			},
+		},
+		parsing: {
+			xAxisKey: "created_at",
+			yAxisKey: "price",
+		},
+		responsive: true,
+	};
 
 	useEffect(() => {
 		const getData = async () => {
 			setConfig((await (await fetch(`${serverUrl}/config/${props.configId}`)).json()) as IConfig);
-			setCheapestProducts(
-				(await (await fetch(`${serverUrl}/cheapest-products/${props.configId}`)).json()) as IProduct[]
-			);
+			const cheapestProducts = (await (
+				await fetch(`${serverUrl}/cheapest-products/${props.configId}`)
+			).json()) as IProduct[];
+
+			const data = cheapestProducts.map((product) => {
+				return {
+					...product,
+					created_at: new Date(product.created_at).toLocaleString(),
+				};
+			});
+
+			setchartData({ datasets: [{ data }] });
+		};
+
+		const sse = new EventSource(`${serverUrl}/sse/${props.configId}`, { withCredentials: true });
+
+		sse.onmessage = (e) => getData();
+		sse.onerror = () => {
+			sse.close();
 		};
 
 		getData();
 	}, []);
 
-	// const handleCheckbox = (vendorId: number) => {
-	// 	const newSelectedVendors = config.selected_vendors ? [...config.selected_vendors] : [];
-	// 	if (newSelectedVendors.includes(vendorId)) newSelectedVendors.splice(newSelectedVendors.indexOf(vendorId), 1);
-	// 	else newSelectedVendors.push(vendorId);
-
-	// 	handleChange("selected_vendors", newSelectedVendors);
-	// };
-
-	// const handleChange = (key: string, value: string | number[] | number) => {
-	// 	setConfig((previousConfig) => ({
-	// 		...previousConfig,
-	// 		[key]: value,
-	// 	}));
-	// };
-
-	// const onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-	// 	event.preventDefault();
-
-	// 	props.onSubmit(config);
-	// };
-
-	// return (
-	// 	<form className="form form-config" onSubmit={(event) => onFormSubmit(event)}>
-	// 		<fieldset>
-	// 			<label htmlFor="search_term">Searchterm</label>
-	// 			<input
-	// 				type="text"
-	// 				name="search_term"
-	// 				id="search_term"
-	// 				placeholder="3070Ti"
-	// 				value={config.search_term}
-	// 				onChange={(event) => {
-	// 					handleChange(event.target.name, event.target.value);
-	// 				}}
-	// 			/>
-	// 		</fieldset>
-	// 		<fieldset>
-	// 			<label htmlFor="search_frequency">Search frequency</label>
-	// 			<input
-	// 				type="text"
-	// 				name="search_frequency"
-	// 				id="search_frequency"
-	// 				placeholder="hh:mm"
-	// 				value={config.search_frequency}
-	// 				onChange={(event) => {
-	// 					handleChange(event.target.name, event.target.value);
-	// 				}}
-	// 			/>
-	// 		</fieldset>
-	// 		<fieldset>
-	// 			<select
-	// 				name="category_id"
-	// 				onChange={(event) => {
-	// 					handleChange(event.currentTarget.name, parseInt(event.currentTarget.value));
-	// 				}}
-	// 			>
-	// 				{config.id ? null : <option id="null">Select Category</option>}
-	// 				{categories.map((category) => (
-	// 					<option
-	// 						id={category.id.toString()}
-	// 						selected={category.id === config.category_id ? true : false}
-	// 						value={category.id}
-	// 					>
-	// 						{category.name}
-	// 					</option>
-	// 				))}
-	// 			</select>
-	// 		</fieldset>
-	// 		<div className="checkbox-vendor">
-	// 			{vendors.map((vendor) => (
-	// 				<fieldset key={vendor.id}>
-	// 					<label htmlFor={vendor.id + ""}>{vendor.name}</label>
-	// 					<input
-	// 						type="checkbox"
-	// 						id={vendor.id + ""}
-	// 						name={vendor.id + ""}
-	// 						checked={config.selected_vendors?.includes(vendor.id)}
-	// 						onChange={() => handleCheckbox(vendor.id)}
-	// 					></input>
-	// 				</fieldset>
-	// 			))}
-	// 		</div>
-
-	// 		<input type="submit" className="button button-submit" value={props.configId ? "Save" : "Create"}></input>
-	// 	</form>
-	// );
-	return (
-		<div>
-			{cheapestProducts.map((product) => (
-				<span>product</span>
-			))}
-		</div>
-	);
+	if (chartData?.datasets?.length)
+		return <Line datasetIdKey={props.configId.toString()} data={chartData} options={options}></Line>;
+	return <span></span>;
 };
 
 export default Chart;
