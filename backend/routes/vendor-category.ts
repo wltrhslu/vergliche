97a313cc0f9df7dff3550ad6a9119cfc53a83032
Mirco <mirco.walter@stud.hslu.ch>
2022-03-03 19:09:00 +0100

    lock oak version

--- backend/routes/vendor-category.ts
+++ backend/routes/vendor-category.ts
@@ -1,4 +1,4 @@
-import { Router, Status } from "https://deno.land/x/oak/mod.ts";
+import { Router, Status } from "https://deno.land/x/oak@v10.4.0/mod.ts";
 import vendorCategoryController from "../controllers/vendor-category.ts";
 
 const router = new Router();
@@ -11,10 +11,7 @@ router.get("/vendor-category", async (context) => {
 		context.response.status = Status.OK;
 	} catch (error) {
 		context.response.status = 500;
-		context.response.body = JSON.stringify(
-			error,
-			Object.getOwnPropertyNames(error)
-		);
+		context.response.body = JSON.stringify(error, Object.getOwnPropertyNames(error));
 		console.log(error);
 	}
 });
