625036a1347ada2e3e60e45411473da34b3047f7
wltrhslu <mirco.walter@gmail.com>
2022-03-09 19:58:52 +0100

    dev db

--- backend/main.ts
+++ backend/main.ts
@@ -1,6 +1,11 @@
 import { DatabaseService } from "./services/Database.ts";
 
-const db = new DatabaseService("vergliche", "wltr.internet-box.ch", "root", "Welcome01");
+const db = new DatabaseService(
+	Deno.cwd() === "/" ? "vergliche" : "vergliche_dev",
+	"wltr.internet-box.ch",
+	"root",
+	"Welcome01"
+);
 await db.initDatabase();
 
 import { Application, Router, send } from "https://deno.land/x/oak@v10.4.0/mod.ts";
