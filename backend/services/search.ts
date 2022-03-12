699ef195ebb1689e2934ce0400ea2ba28dbbfec1
wltrhslu <mirco.walter@gmail.com>
2022-03-12 02:01:51 +0100

    disable steg again, deno dom doesn't want to load on rpi

--- backend/services/search.ts
+++ backend/services/search.ts
@@ -10,7 +10,7 @@ export default class SearchService {
 	sseTargets: { configId: number; target: ServerSentEventTarget }[];
 
 	constructor() {
-		this.searchServices = [new Digitec(), new StegElectronics()];
+		this.searchServices = [new Digitec()];
 		this.sseTargets = new Array<{ configId: number; target: ServerSentEventTarget }>();
 	}
 
